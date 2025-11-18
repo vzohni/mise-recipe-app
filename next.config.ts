2fe1287fc7804b5fa4cdc322ec39d72fa15f170b
Victor Zohni <vzohni@gmail.com>
2025-11-18 17:50:26 -0500

    fix nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Disable ESLint during build
   },
   typescript: {
     ignoreBuildErrors: true, // Ignore TypeScript errors during build
   },
-};
 };
 
 export default nextConfig;
